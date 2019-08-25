51b948f44129ebcc04c98ea495dd280a09a59f32
Ellen <eli030@uottawa.ca>
2019-08-25 09:50:13 -0400

    kms'

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -7,25 +7,52 @@ import Colors from '../constants/Colors';
 
 import { connect } from 'react-redux';
 import { sendMessage } from '../actions/actions';
+import { Predictions } from 'aws-amplify';
 
 class ChatScreen extends React.Component {
   constructor(props) {
     super(props);
 
-    state = {
-      messages: []
+    this.state = {
+      messages: [],
+      response: '',
+      textToInterpret: '',
+      negative: 0,
+      modalVisible: false
     };
 
     this.onSend = this.onSend.bind(this);
   }
 
+  interpretFromPredictions = textToInterpret => {
+    textToInterpret
+      ? Predictions.interpret({
+          text: {
+            source: {
+              text: textToInterpret,
+              language: 'en'
+            },
+            type: 'SENTIMENT'
+          }
+        })
+          .then(result =>
+            this.setState({ response: JSON.stringify(result, null, 2) })
+          )
+          .catch(err =>
+            this.setState({ response: JSON.stringify(err, null, 2) })
+          )
+      : null;
+  };
+
+  setText = text => {
+    this.setState({ textToInterpret: text });
+  };
+
   static navigationOptions({ navigation }) {
     return {
       headerTitle: navigation.state.params.name,
       headerRight: (
-        <TouchableOpacity
-          onPress={() => navigation.navigate('Trends')}
-        >
+        <TouchableOpacity onPress={() => navigation.navigate('Trends')}>
           <Icon
             name={
               Platform.OS === 'ios'
@@ -33,7 +60,7 @@ class ChatScreen extends React.Component {
                 : 'md-information-circle-outline'
             }
             size={28}
-            color='grey'
+            color="grey"
             style={{ paddingRight: 16 }}
           />
         </TouchableOpacity>
@@ -50,6 +77,10 @@ class ChatScreen extends React.Component {
   }
 
   onSend(messages = []) {
+    this.setState({
+      textToInterpret: messages[0].text
+    });
+
     this.props.sendMessage(
       this.props.navigation.state.params.username,
       messages[0]._id,
@@ -63,9 +94,22 @@ class ChatScreen extends React.Component {
         ...messages[0].user
       }
     );
+
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages)
     }));
+    this.interpretFromPredictions(this.state.textToInterpret);
+    console.log("response" + this.state.response);
+    this.state.response
+      ? this.setState({
+          negative: Number(
+            JSON.parse(this.state.response).textInterpretation.sentiment
+              .negative
+          )
+        })
+      : null;
+
+    this.state.negative > 0.6 ? alert("This message has been detected as overly negative. Please use more positive words in the future.") : null;
   }
 
   renderBubble(props) {
@@ -80,7 +124,7 @@ class ChatScreen extends React.Component {
       />
     );
   }
-  
+
   render() {
     return (
       <View style={{ flex: 1 }}>
@@ -91,11 +135,18 @@ class ChatScreen extends React.Component {
             _id: 1
           }}
           renderBubble={this.renderBubble}
-          onLongPress={(ctx, currentMessage) => this.props.navigation.navigate('MsgSent', {
+          onLongPress={(ctx, currentMessage) =>
+            this.props.navigation.navigate('MsgSent', {
               msg: currentMessage.text
-            })}
+            })
+          }
         />
         <KeyboardSpacer topSpacing={-50} />
+        {this.state.modalVisible ? (
+          <StyledModal visible={true} index={this.state.currentIndex} />
+        ) : (
+          <View />
+        )}
       </View>
     );
   }
